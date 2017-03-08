dbb6cd37d4df7f0b17847a59004143f9fd566ffc
Ben Schenkengerger <benschenkenberger@gmail.com>
2017-03-08 08:29:33 -0800

    add missing brackets to if statements

--- js/scripts.js
+++ js/scripts.js
@@ -5,56 +5,59 @@
 var totalCost;
 
 // pizza constructor
-function Pizza(size, toppings, price) {
+function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
-  this.price = 0;
 }
 
-function Toppings(name, price) {
-  this.name = name;
-  this.price = .75;
-}
-
-var tomatoes = {
-  name: "tomatoes",
-  price: .75;
-}
-
-var sausage = {
-  name: "sausage",
-  price: .75;
-}
-
-var pepperoni = {
-  name: "pepperoni",
-  price: .75;
-}
-
-var pineapple = {
-  name: "pineapple",
-  price: .75;
-}
-
-var cheddar = {
-  name: "cheddar",
-  price: .75;
-}
+// function Toppings(name, price) {
+// //   this.name = name;
+// //   this.price = .75;
+// // }
+//
+// var tomatoes = {
+//   name: "tomatoes",
+//   price: .75;
+// }
+//
+// var sausage = {
+//   name: "sausage",
+//   price: .75;
+// }
+//
+// var pepperoni = {
+//   name: "pepperoni",
+//   price: .75;
+// }
+//
+// var pineapple = {
+//   name: "pineapple",
+//   price: .75;
+// }
+//
+// var cheddar = {
+//   name: "cheddar",
+//   price: .75;
+// }
 
 var customerPizza = new Pizza;
 
 
-
-Pizza.prototype.fullPrice = function() {
-  this.toppings.forEach(function(topping) {
-    this.price += .75;
-  } if this.size === "extraLarge" {
-    this.price += 14.99;
-  } if else this.size === "large" {
-    this.price += 12.99;
-  } if else this.size === "medium" {
-    this.price += 9.99;
+Pizza.prototype = {
+  fullPrice: function(){
+  var fullPrice=0;
+    if (this.size === "extraLarge") {
+    fullPrice = 14.99;
+  } if else (this.size === "large") {
+    fullPrice = 12.99;
+  } if else (this.size === "medium") {
+    fullPrice = 9.99;
   } else {
-    this.price += 7.99;
-  })
+    fullPrice = 7.99;
+  }
+    this.toppings.forEach(function(topping) {
+    fullPrice += .75;
+    }
+    return fullPrice;
+  }
 };
