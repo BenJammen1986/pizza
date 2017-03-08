5c7c084ce5613a85e6bc628bb30d84aab149929a
Ben Schenkengerger <benschenkenberger@gmail.com>
2017-03-08 09:10:28 -0800

    add working notes

--- js/scripts.js
+++ js/scripts.js
@@ -2,49 +2,17 @@
 // business logic
 
 
-var totalCost;
-
 // pizza constructor
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 }
 
-// function Toppings(name, price) {
-// //   this.name = name;
-// //   this.price = .75;
-// // }
-//
-// var tomatoes = {
-//   name: "tomatoes",
-//   price: .75;
-// }
-//
-// var sausage = {
-//   name: "sausage",
-//   price: .75;
-// }
-//
-// var pepperoni = {
-//   name: "pepperoni",
-//   price: .75;
-// }
-//
-// var pineapple = {
-//   name: "pineapple",
-//   price: .75;
-// }
-//
-// var cheddar = {
-//   name: "cheddar",
-//   price: .75;
-// }
 
 var customerPizza = new Pizza;
 
 
-Pizza.prototype = {
-  fullPrice: function(){
+Pizza.prototype.fullPrice = function() {
   var fullPrice=0;
     if (this.size === "extraLarge") {
     fullPrice = 14.99;
@@ -57,7 +25,6 @@ Pizza.prototype = {
   }
     this.toppings.forEach(function(topping) {
     fullPrice += .75;
-    }
+    })
     return fullPrice;
-  }
 };
