d556f408f461685a440677889ae0c4325b4ce9d5
Ben <benschenkenberger@gmail.com>
2017-03-08 13:56:37 -0800

    create front end logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,35 +2,49 @@
 // business logic
 
 // pizza constructor
+
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 }
 
-var customerPizza = new Pizza();
-
-
 Pizza.prototype.fullPrice = function() {
-  var fullPrice=0;
+    var Price=0;
     if (this.size === "extraLarge") {
-    fullPrice = 14.99;
-  } else if (this.size === "large") {
-    fullPrice = 12.99;
-  } else if (this.size === "medium") {
-    fullPrice = 9.99;
+    Price = 14.99;
+  } else if (this.size === "Large") {
+    Price = 12.99;
+  } else if (this.size === "Medium") {
+    Price = 9.99;
   } else {
-    fullPrice = 7.99;
+    Price = 7.99;
   }
     this.toppings.forEach(function(topping) {
-    fullPrice += .75;
+    Price += .75;
     })
-    return fullPrice;
-};
-
-
+    return Price;
+}
 
 // user interface logic:
 
 $(function() {
-  ("#formOne")
-})
+  $("#allInfo").submit(function(event) {
+    debugger;
+    var allToppings = [];
+    event.preventDefault();
+    $("#price").show();
+// All input fields
+    var inputSize = $("input[name=pizzaSize]").val();
+    $("input:checkbox[name=pizzaToppings]:checked").map(function(){
+      allToppings = ($(this).val());
+    })
+// Object creation
+    var customerPizza = new Pizza(inputSize,allToppings);
+// Output field
+    // $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+    $("#orderDetails").append("<li><b>Size:</b>" + customerPizza.size + "</li>");
+    // $("orderDetails").append.forEach("")
+    $("#orderDetails").append("<li><b>Price:</b>") + customerPizza.fullPrice + "</li>"
+
+  });
+});
